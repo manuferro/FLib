f4b4ccf2c0f60aaf47a32dffcb644609ce88d186
manuferro <ferro1@libero.it>
2020-04-05 15:52:19 +0200

    Aggiungere i file di progetto.

---
+++ FLib/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FLib
+{
+    public class HelloWorld
+    {
+        private const string HELLO_MESSAGE = "Hello world MF!";
+        public string getHello()
+        {
+            return HELLO_MESSAGE;
+        }
+    }
+
+
+}
