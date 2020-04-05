82caf5c54be6b51ae24d2a9264cdbbc6f455a23d
manuferro <ferro1@libero.it>
2020-04-05 16:30:51 +0200

    test hello world

--- FLib/HelloWorld.cs
+++ FLib/HelloWorld.cs
@@ -2,9 +2,12 @@
 
 namespace FLib
 {
+    /// <summary>
+    /// class to test gitHub functionalities
+    /// </summary>
     public class HelloWorld
     {
-        private const string HELLO_MESSAGE = "Hello world MF!";
+        private const string HELLO_MESSAGE = "Hello world MF!!!";
         public string getHello()
         {
             return HELLO_MESSAGE;
